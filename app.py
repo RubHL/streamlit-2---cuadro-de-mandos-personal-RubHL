debb9f8652c4986ca71d273888991c65edc37d83
Ru <you@example.com>
2023-02-28 03:08:38 +0100

    inicio

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
-import pandas as pd
 import streamlit as st
+import pandas as pd
+
+
+# URL con la lista de pasajeros del Titanic
+# https://github.com/KeithGalli/pandas/blob/master/pokemon_data.csv
+
+
+url = "https://raw.githubusercontent.com/KeithGalli/pandas/master/pokemon_data.csv"
+df = pd.read_csv(url)
+
+sorted_df = df.sort_values("HP")
+
+st.write(sorted_df)
